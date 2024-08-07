9c23f0d2afd6deab2788387731430f1ec6f7a1e8
tauyoung <tauyoung@icloud.com>
2024-08-07 09:25:20 +0800

    Initial Commit

---
+++ src/test/java/com/laioffer/homehub/HomehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laioffer.homehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
